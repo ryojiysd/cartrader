32f3015409ae7fb0712115370a4279be0c1ae475
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-28 10:40:28 +0900

    Building the search bar

--- app.vue
+++ app.vue
@@ -1,24 +1,3 @@
 <template>
-  <div>
-    <!-- NAAVBAR-->
-    <header
-      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-    >
-      <a class="text-3xl font-mono" href="">cartrader</a>
-    </header>
-    <!-- NAAVBAR-->
-
-    <!-- HOME HERO -->
-    <div
-      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://wallup.net/wp-content/uploads/2019/09/586471-2013-ferrari-laferrari-supercar.jpg')]"
-    >
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2">
-          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
-        </div>
-      </div>
-      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
-    </div>
-    <!-- HOME HERO -->
-  </div>
+  <div></div>
 </template>
