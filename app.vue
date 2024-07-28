3801a9ff5e818089e771e12b3515c147a0094632
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-28 10:33:00 +0900

    Building the home hero

--- app.vue
+++ app.vue
@@ -9,5 +9,16 @@
     <!-- NAAVBAR-->
 
     <!-- HOME HERO -->
+    <div
+      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://wallup.net/wp-content/uploads/2019/09/586471-2013-ferrari-laferrari-supercar.jpg')]"
+    >
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+    <!-- HOME HERO -->
   </div>
 </template>
