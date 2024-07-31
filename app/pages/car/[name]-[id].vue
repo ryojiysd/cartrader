087cf1015648c1eb0259473c3451c8eb05429dcb
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-07-31 23:16:12 +0900

    Storing common functionality in composables

--- app/pages/car/[name]-[id].vue
+++ app/pages/car/[name]-[id].vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-    >
-      <CarDetailHero />
-      <CarDetailAttributes />
-      <CarDetailDescription />
-      <CarDetailContact />
-    </div>
+    <CarDetailHero />
+    <CarDetailAttributes />
+    <CarDetailDescription />
+    <CarDetailContact />
   </div>
 </template>
 
 <script setup>
-const toTitleCase = (str) => {
-  return str.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-};
-
 const route = useRoute();
+const { toTitleCase } = useUtilities();
+
 useHead({
   title: toTitleCase(route.params.name),
+});
+
+definePageMeta({
+  layout: "custom",
 });
 </script>
