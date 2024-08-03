70383614237780c128294b90e904f38e39b40700
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-03 09:56:58 +0900

    Dynamically rendering the car cards

--- app/components/Car/Card.vue
+++ app/components/Car/Card.vue
@@ -1,20 +1,24 @@
 <template>
   <div
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    ,
+    @click="navigateTo(`/car/${car.name}-${car.id}`)"
   >
     <div class="flex h-full">
-      <img
-        src="http://i0.wp.com/autordee.com/wp-content/uploads/2015/11/SSC-Ultimate-Aero-1.jpg"
-        alt=""
-        class="w-[300px] h-full"
-      />
+      <img :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Volvo</h1>
-          <p class="text-gray-700">Lorem ipsum dolor...</p>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          <p class="text-gray-700">{{ car.description }}</p>
         </div>
-        <h1 class="mt-auto text-xl">$39.555</h1>
+        <h1 class="mt-auto text-xl">{{ car.price }}</h1>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
