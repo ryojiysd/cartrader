f3696d3b22b709d5ba317703b6bfcce138daf065
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-03 12:14:54 +0900

    The useState composable

--- app/components/Car/Card.vue
+++ app/components/Car/Card.vue
@@ -1,10 +1,14 @@
 <template>
   <div
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    ,
-    @click="navigateTo(`/car/${car.name}-${car.id}`)"
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
-    <div class="flex h-full">
+    <img
+      class="absolute w-7 right-5 top-2 z-20"
+      :src="favored ? heartFilled : heartOutline"
+      alt=""
+      @click="favored = !favored"
+    />
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
       <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
@@ -18,7 +22,12 @@
 </template>
 
 <script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+
 const props = defineProps({
   car: Object,
 });
+
+const favored = useState(`favored-${props.car.id}`, () => false);
 </script>
