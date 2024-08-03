0101c2a9d6c54f62a1f4d72bf29186103761da48
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-03 10:11:36 +0900

    Dynamically rendering the car details page

--- app/pages/car/[name]-[id].vue
+++ app/pages/car/[name]-[id].vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
-    <CarDetailHero />
-    <CarDetailAttributes />
-    <CarDetailDescription />
+  <div v-if="car">
+    <CarDetailHero :car="car" />
+    <CarDetailAttributes :features="car.features" />
+    <CarDetailDescription :description="car.description" />
     <CarDetailContact />
   </div>
 </template>
 
 <script setup>
 const route = useRoute();
+const { cars } = useCars();
 const { toTitleCase } = useUtilities();
 
 useHead({
   title: toTitleCase(route.params.name),
+});
+
+const car = computed(() => {
+  return cars.find((c) => c.id === parseInt(route.params.id));
 });
 
 definePageMeta({
