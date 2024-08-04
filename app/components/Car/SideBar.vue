94276fa0ab8d370f738af1e1e3f6c34f007b0497
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-04 12:07:21 +0900

    Adding the car price filter

--- app/components/Car/SideBar.vue
+++ app/components/Car/SideBar.vue
@@ -43,10 +43,38 @@
     </div>
     <!-- MAKE END -->
 
+    <!-- PRICE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize">Any</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+        {{ priceRangeText }}
+      </h3>
+      <h3 class="text-blue-400 capitalize"></h3>
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+        v-if="modal.price"
+      >
+        <input
+          class="border p-1 rounded"
+          type="number"
+          placeholder="Min"
+          v-model="priceRange.min"
+        />
+        <input
+          class="border p-1 rounded"
+          type="number"
+          placeholder="Max"
+          v-model="priceRange.max"
+        />
+        <button
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+          @click="onChangePrice"
+        >
+          Apply
+        </button>
+      </div>
     </div>
+    <!-- PRICE END -->
   </div>
 </template>
 
@@ -59,7 +87,21 @@ const modal = ref({
 });
 
 const city = ref("");
+const priceRange = ref({
+  min: "",
+  max: "",
+});
 const route = useRoute();
+const router = useRouter();
+
+const priceRangeText = computed(() => {
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+  if (!minPrice && !maxPrice) return "Any";
+  else if (!minPrice && maxPrice) return `< $${maxPrice}`;
+  else if (minPrice && !maxPrice) return `> $${minPrice}`;
+  else return `$${minPrice} - $${maxPrice}`;
+});
 
 const updateModal = (key) => {
   modal.value[key] = !modal.value[key];
@@ -81,5 +123,18 @@ const onChangeLocation = () => {
 const onChangeMake = (make) => {
   updateModal("make");
   navigateTo(`/city/${route.params.city}/car/${make}`);
+};
+
+const onChangePrice = () => {
+  updateModal("price");
+  if (priceRange.value.max && priceRange.value.min) {
+    if (priceRange.value.min > priceRange.value.max) return;
+  }
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max,
+    },
+  });
 };
 </script>
