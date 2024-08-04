b6a50ab62179bea2976e49e931a8de5203a77db3
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-04 10:43:38 +0900

    Forcing client side rendering

--- app/components/Car/Cards.vue
+++ app/components/Car/Cards.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="w-full">
-    <CarCard
-      v-for="car in cars"
-      :key="car.id"
-      :car="car"
-      @favor="handleFavorite"
-      :favored="car.id in favorite"
-    />
+    <ClientOnly>
+      <CarCard
+        v-for="car in cars"
+        :key="car.id"
+        :car="car"
+        @favor="handleFavorite"
+        :favored="car.id in favorite"
+      />
+    </ClientOnly>
   </div>
 </template>
 
