1b6e8c1161d94c27b5bc951e3d3b1f559eb93715
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-10 14:16:09 +0900

    Extracting the query parameters

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -2,9 +2,28 @@ import cars from "@/data/cars.json";
 
 export default defineEventHandler((event) => {
   const { city } = event.context.params;
+  const { make, minPrice, maxPrice } = getQuery(event);
 
-  const filteredCars = cars.filter(
+  let filteredCars = cars.filter(
     (car) => car.city.toLowerCase() === city.toLowerCase()
   );
+
+  if (make) {
+    filteredCars = filteredCars.filter(
+      (car) => car.make.toLowerCase() === make.toLowerCase()
+    );
+  }
+
+  if (minPrice) {
+    filteredCars = filteredCars.filter(
+      (car) => parseInt(minPrice) <= car.price
+    );
+  }
+
+  if (maxPrice) {
+    filteredCars = filteredCars.filter(
+      (car) => car.price <= parseInt(maxPrice)
+    );
+  }
   return filteredCars;
 });
