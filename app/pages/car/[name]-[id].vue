19fa297240d4b43cc86146bb40307e0cc62ccc3a
Ryoji Yoshida <ryoji.ysd@gmail.com>
2024-08-11 10:31:20 +0900

    Another fetch composable

--- app/pages/car/[name]-[id].vue
+++ app/pages/car/[name]-[id].vue
@@ -9,23 +9,13 @@
 
 <script setup>
 const route = useRoute();
-const { cars } = useCars();
+const car = await useFetchCar(route.params.id);
+
 const { toTitleCase } = useUtilities();
 
 useHead({
   title: toTitleCase(route.params.name),
 });
-
-const car = computed(() => {
-  return cars.find((c) => c.id === parseInt(route.params.id));
-});
-
-if (!car.value) {
-  throw createError({
-    statusCode: 404,
-    message: `Car with ID of ${route.params.id} does not exist`,
-  });
-}
 
 definePageMeta({
   layout: "custom",
